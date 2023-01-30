80cd4841f0d4e824810f0ad6ae3c413f86180dd1
Pavel Sidorov <pavel.o.sidorov@gmail.com>
2023-01-30 17:57:16 +0900

    Updated Chromatogram file

--- HPLCAnalysis/chromatogram.py
+++ HPLCAnalysis/chromatogram.py
@@ -316,3 +316,23 @@ class Chromatogram:
             if to_exclude not in self.peaks.keys():
                 return 1.
             return 1.-self.ratios[to_exclude]
+
+    def _optional(self, value, default):
+    """Helper function that sets the optional value with given or default.
+
+    Parameters
+    ----------
+    value : Any
+        the argument of the function that is checked against None
+    default : Any
+        A default value to return if the given argument is None
+
+    Returns
+    -------
+    Any
+        given value, or default value if None is given
+    """
+    if value is None:
+        return default
+    else:
+        return value
